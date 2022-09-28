0aeb34e8d609ef701e34df20043ca12b58410572
justinbiebergoth <hiirari666@gmail.com>
2022-09-28 23:01:31 +0300

    refactoring

--- ColourChanger/ViewController.swift
+++ ColourChanger/ViewController.swift
@@ -17,46 +17,52 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
     @IBOutlet var redValue: UILabel!
     @IBOutlet var greenValue: UILabel!
     @IBOutlet var blueValue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colourView.layer.cornerRadius = colourView.frame.width / 10
+        colourView.layer.cornerRadius =  20
+        setValueText()
+        setupColour()
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "1134751")
         backgroundImage.contentMode = .scaleAspectFill
         view.insertSubview(backgroundImage, at: 0)
-        
     }
     
     //MARK: - IB Actions
     
-    @IBAction func chagingColours() {
-        colourView.backgroundColor = UIColor(
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setupColour()
+        setValueText()
+        switch sender {
+        case redSlider:
+            redValue.text = string(from: sender)
+        case greenSlider:
+            greenValue.text = string(from: sender)
+        default:
+            blueValue.text = string(from: sender)
+        }
+  
+    }
+    
+    private func setValueText() {
+        redValue.text = string(from: redSlider)
+        greenValue.text = string(from: greenSlider)
+        blueValue.text = string(from: blueSlider)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+
+    private func setupColour() {
+            colourView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1)
     }
-    
-    @IBAction func redSliderAction() {
-        redValue.text = floatToString(from: redSlider)
-        
-    }
-    
-    @IBAction func greenSliderAction() {
-        greenValue.text = floatToString(from: greenSlider)
-    }
-    
-    @IBAction func blueSliderAction() {
-        blueValue.text = floatToString(from: blueSlider)
-    }
-    
-    private func floatToString(from slider: UISlider) -> String {
-        String(format: "%.2f", slider.value)
-    }
-
 }
